578ebf83d8f24f161cc798e49ef3d8406c172b53
Alex Weibel <aweibel@amazon.com>
2017-03-29 12:17:22 -0700

    Move gcc6 installation to .travis.yml

--- .travis/install_ubuntu_dependencies.sh
+++ .travis/install_ubuntu_dependencies.sh
@@ -15,9 +15,6 @@
 
 set -e
 
-if [[ "$GCC6_REQUIRED" == "true" ]]; then
-    sudo apt-get -y install gcc-6 c++-6 ; fi
-
 # Download and Install prlimit for memlock
 if [[ ! -d "$PRLIMIT_INSTALL_DIR" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     mkdir -p $PRLIMIT_INSTALL_DIR && sudo .travis/install_prlimit.sh `mktemp -d` $PRLIMIT_INSTALL_DIR > /dev/null ; fi
