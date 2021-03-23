6c0785829b9123711a40433044fd16cea55cbbfb
Alex Weibel <aweibel@amazon.com>
2021-03-23 16:00:11 -0700

    Fix CMake issues

---
+++ pq-crypto/bike_r3/bike-kem/cmake/bike-config.cmake
@@ -0,0 +1,14 @@
+include(CMakeFindDependencyMacro)
+
+if (NOT MSVC)
+    set(THREADS_PREFER_PTHREAD_FLAG ON)
+    find_package(Threads REQUIRED)
+endif()
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
+
+if (BUILD_SHARED_LIBS)
+    include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
+else()
+    include(${CMAKE_CURRENT_LIST_DIR}/static/@PROJECT_NAME@-targets.cmake)
+endif()
